964804c3209de579341e9360f2cecf0f7a8ef15d
Adam Wolski <miniukof@gmail.com>
2016-03-13 17:40:35 +0100

    Add init file so you can use addon from zip file.

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from . import screen_pie
+
+import bpy
+
+bl_info = {'name': 'Screen Pie Menu',
+           'description': 'Select screen using pie menu.',
+           'author': 'miniukof',
+           'version': (0, 0, 2),
+           'blender': (2, 76, 11),
+           'category': 'User Interface',
+           'wiki_url': 'https://github.com/miniukof/bl-screen_pie'
+          }
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
