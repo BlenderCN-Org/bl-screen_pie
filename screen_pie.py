81f1905f5ae43f1b8c1e711c2c44b2451184a1be
miniukof <miniukof@gmail.com>
2016-08-09 11:16:16 +0200

    Use bl_info in init only

--- screen_pie.py
+++ screen_pie.py
@@ -1,14 +1,5 @@
 import bpy
 
-bl_info = {'name': 'Screen Pie Menu',
-           'description': 'Select screen using pie menu.',
-           'location' : "Add shortcut to `ui.screen_pie_menu`",
-           'author': 'miniukof',
-           'version': (0, 0, 2),
-           'blender': (2, 76, 11),
-           'category': 'User Interface',
-           'wiki_url': 'https://github.com/miniukof/bl-screen_pie'
-          }
 
 class ScreenPieMenu(bpy.types.Menu):
     bl_label = 'Screen Pie Menu'
